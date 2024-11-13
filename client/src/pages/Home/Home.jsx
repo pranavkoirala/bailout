a240988add7d4abbf322b041639c73f91262b049
Pranav Koirala <pranavkoirala7@gmail.com>
2024-11-12 20:10:57 -0800

    Refactor Home component to use Footer component and update background color for improved aesthetics

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import { useNavigate } from "react-router-dom";
-
+import Footer from "../../assets/Footer";
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col justify-center items-center bg-[#111000] h-screen text-white font-mono text-center w-screen">
+    <div className="flex flex-col justify-center items-center bg-[#010101] h-screen text-white font-mono text-center w-screen">
       <h1 className="text-2xl">bailout</h1>
       <p className="text-lg mt-[1rem]">
         set plans... more importantly, cancel plans
@@ -16,23 +16,7 @@ const Home = () => {
         create
       </button>
 
-      <footer className="flex flex-col items-center absolute bottom-10">
-        <p className="mb-4 bg-gradient-to-r from-[#830dd9] to-pink-500 bg-clip-text text-transparent h-fit text-xs">
-          bailout is made with love by{" "}
-          <a
-            href="https://github.com/pranavkoirala"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="relative inline-block text-black rounded-lg px-2 py-1 group"
-          >
-            <span
-              className="absolute inset-0 rounded-lg bg-gradient-to-r from-white to-blue-200 blur-sm opacity-75 group-hover:opacity-100 transition duration-800 group-hover:duration-200 animate-tilt"
-              aria-hidden="true"
-            ></span>
-            <span className="relative">pranav</span>
-          </a>
-        </p>
-      </footer>
+      <Footer />
     </div>
   );
 };
