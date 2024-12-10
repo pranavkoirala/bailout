55c8bb03a6693133213354dc726f1c49936fa205
pkrla <110214889+pranavkoirala@users.noreply.github.com>
2024-12-10 13:16:57 -0800

    update home page message with downtime

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -9,6 +9,11 @@ const Home = () => {
       <p className="text-lg mt-[1rem]">
         set plans... more importantly, cancel plans
       </p>
+      <p className="text-lg mt-[1rem]">
+        bailout is currently down for winter break because i will be away from
+        campus and don't want to spend the money to keep the server running for
+        a wasted month.
+      </p>
       <button
         onClick={() => navigate("/create")}
         className="mt-[2rem] bg-[#292929] border-2 border-[#3e3e3e] rounded-lg text-white px-6 py-3 text-base hover:border-[#fff] cursor-pointer transition"
